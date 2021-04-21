72b50707f331ab8c5933635a4a025cd08a4dc049
Marvin <marv42+github@gmail.com>
2021-04-21 18:08:46 +0200

    save cookies
    ignore weblogon

--- StadtBibliothek.py
+++ StadtBibliothek.py
@@ -1,12 +1,15 @@
+import logging
+from logging import DEBUG
+import pickle
 import re
 import subprocess
 from datetime import timedelta, datetime
 
 import requests
 from bs4 import BeautifulSoup
-from LoginData import CARD_NUMBER, PASSWORD, COOKIES, MAIL_ADDRESS
+from LoginData import CARD_NUMBER, PASSWORD, MAIL_ADDRESS
 
-WARN_DAYS_IN_ADVANCE = 3
+WARN_DAYS_IN_ADVANCE = 7
 OPAC_URL = "http://opac.st-ingbert.de/webopac/index.asp"
 DATA = {'kontofenster': 'true',
         'AUSWEIS': CARD_NUMBER,
@@ -14,11 +17,38 @@ DATA = {'kontofenster': 'true',
         'B1': '%A0%A0%A0Weiter%A0%A0%A0',
         'target': 'konto',
         'type': 'K'}
+COOKIES_FILE = "cookies.pickle"
 
 
 def get_html():
-    result = requests.post(OPAC_URL, data=DATA, cookies=COOKIES)
+    cookies = handle_cookies()
+    result = requests.post(OPAC_URL, data=DATA, cookies=cookies)
     return result.text
+
+
+def handle_cookies():
+    cookies = load_cookies()
+    result = requests.post(OPAC_URL, data=DATA, cookies=cookies)
+    add_new_cookies(cookies, result.cookies)
+    save_cookies(cookies)
+    logging.debug(cookies)
+    return cookies
+
+
+def load_cookies():
+    with open(COOKIES_FILE, 'rb') as f:
+        return pickle.load(f)
+
+
+def save_cookies(cookies):
+    with open(COOKIES_FILE, 'wb') as f:
+        pickle.dump(cookies, f)
+
+
+def add_new_cookies(cookies, result_cookies):
+    new_cookies = result_cookies.items()
+    for cookie in new_cookies:
+        cookies[cookie[0]] = cookie[1]
 
 
 def get_books(html):
@@ -37,10 +67,16 @@ def is_in_less_than_x_days(date_in_question, x):
     return today + timedelta(days=x) > date_in_question
 
 
-html = get_html()
-parsed_html = BeautifulSoup(html, 'lxml')
-books = get_books(parsed_html)
-first_date = get_first_date(books)
+def main():
+    logging.basicConfig(level=DEBUG)
+    html = get_html()
+    parsed_html = BeautifulSoup(html, 'lxml')
+    books = get_books(parsed_html)
+    first_date = get_first_date(books)
 
-if first_date and is_in_less_than_x_days(first_date, WARN_DAYS_IN_ADVANCE):
-    subprocess.run(["mail", "-s", f"Bücher laufen in {WARN_DAYS_IN_ADVANCE} Tagen ab", MAIL_ADDRESS])
+    if first_date and is_in_less_than_x_days(first_date, WARN_DAYS_IN_ADVANCE):
+        subprocess.run(["mail", "-s", f"Bücher laufen in weniger als {WARN_DAYS_IN_ADVANCE} Tagen ab", MAIL_ADDRESS])
+
+
+if __name__ == "__main__":
+    main()
