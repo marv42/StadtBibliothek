4674dfc51c726071e6d2d9fc64933376a5cdd914
Marvin <marv42+github@gmail.com>
2021-04-24 20:19:57 +0200

    log to logfile

--- StadtBibliothek.sh
+++ StadtBibliothek.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-# TODO https://stackoverflow.com/a/246128
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# https://stackoverflow.com/a/246128
 
-python3 $DIR/StadtBibliothek.py
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+LOG_FILE=$DIR/StadtBibliothek.log
+
+JUST_THE_LATEST_PART_OF_THE_LOG_FILE=$( tail -1000 $LOG_FILE )
+echo "$JUST_THE_LATEST_PART_OF_THE_LOG_FILE" > $LOG_FILE
+date >> $LOG_FILE
+echo DIR=$DIR >> $LOG_FILE
+echo SOURCE=$SOURCE >> $LOG_FILE
+cd $DIR
+python3 $DIR/StadtBibliothek.py 2>&1 >> $LOG_FILE
