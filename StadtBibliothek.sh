4d10ceb594b06b2eb00293cd46a14af3f80b69df
Marvin <marv42+github@gmail.com>
2021-04-25 18:54:46 +0200

    touch log file
    if it does not exist

--- StadtBibliothek.sh
+++ StadtBibliothek.sh
@@ -13,6 +13,7 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 LOG_FILE=$DIR/StadtBibliothek.log
+[ ! -f $LOG_FILE ] && touch $LOG_FILE
 
 JUST_THE_LATEST_PART_OF_THE_LOG_FILE=$( tail -1000 $LOG_FILE )
 echo "$JUST_THE_LATEST_PART_OF_THE_LOG_FILE" > $LOG_FILE
