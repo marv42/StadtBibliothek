8e5cb54595fd74cf921131468c1e551089874895
Marvin <marv42+github@gmail.com>
2021-05-18 17:56:51 +0200

    extend_books

--- StadtBibliothek.py
+++ StadtBibliothek.py
@@ -15,20 +15,28 @@ DATA = {'kontofenster': 'true',
         'AUSWEIS': CARD_NUMBER,
         'PWD': PASSWORD,
         'B1': '%A0%A0%A0Weiter%A0%A0%A0',
-        'target': 'konto',
         'type': 'K'}
 COOKIES_FILE = "cookies.pickle"
 
 
+def get_data_with_target(target):
+    data = DATA
+    data['target'] = target
+    return data
+
+
 def get_html():
     cookies = handle_cookies()
-    result = requests.post(OPAC_URL, data=DATA, cookies=cookies)
+    data = get_data_with_target('konto')
+    result = requests.post(OPAC_URL, data=data, cookies=cookies)
     return result.text
 
 
 def handle_cookies():
     cookies = load_cookies()
-    result = requests.post(OPAC_URL, data=DATA, cookies=cookies)
+    data = get_data_with_target('konto')
+    logging.info(f"{len(cookies)} cookies")
+    result = requests.post(OPAC_URL, data=data, cookies=cookies)
     add_new_cookies(cookies, result.cookies)
     save_cookies(cookies)
     logging.debug(cookies)
@@ -51,12 +59,24 @@ def add_new_cookies(cookies, result_cookies):
         cookies[cookie[0]] = cookie[1]
 
 
-def get_books(html):
-    return html.find_all("td")
+def get_books():
+    html = get_html()
+    parsed_html = BeautifulSoup(html, 'lxml')
+    list_of_books = []
+    one_book = []
+    all_tds = parsed_html.find_all("td")
+    for td in all_tds:
+        match = re.search(r'>(.*)</td>', str(td))
+        content = match.group(1)
+        one_book.append(content)
+        if "a href" in content:
+            list_of_books.append(one_book)
+            one_book = []
+    return list_of_books
 
 
-def get_first_date(books):
-    match = re.search(r'\d{2}\.\d{2}\.\d{4}', str(books))
+def get_first_date(something):
+    match = re.search(r'\d{2}\.\d{2}\.\d{4}', str(something))
     if match:
         return datetime.strptime(match.group(0), "%d.%m.%Y")
     return None
@@ -76,14 +96,39 @@ def send_mail(subject, body=''):
         logging.error(f"Error sending mail: return code: {return_code}, stderr: {stderr}")
 
 
+def extend_books(books, end_date):
+    result = ""
+    for book in books:
+        if book[2] == end_date:
+            result += try_extend_book(book)
+    return result
+
+
+def try_extend_book(book):
+    name_and_author = f"{book[1]} ({book[0]})"
+    if "Verlängern" in book[3]:
+        extend()
+        return f"Erfolgreich verlängert: {name_and_author}\n"
+    else:
+        return f"Konnte nicht mehr verlängert werden: {name_and_author}. ABGEBEN!\n"
+
+
+def extend():
+    cookies = load_cookies()
+    data = get_data_with_target('verl_1')
+    requests.post(OPAC_URL, data=data, cookies=cookies)
+    data = get_data_with_target('make_vl')
+    requests.post(OPAC_URL, data=data, cookies=cookies)
+
+
 def main():
     # logging.basicConfig(level=DEBUG)
-    html = get_html()
-    parsed_html = BeautifulSoup(html, 'lxml')
-    books = get_books(parsed_html)
+    books = get_books()
     first_date = get_first_date(books)
     if first_date and is_in_less_than_x_days(first_date, WARN_DAYS_IN_ADVANCE):
-        send_mail("Bücher laufen ab", f"... am {first_date.strftime('%d. %m.')} (in weniger als {WARN_DAYS_IN_ADVANCE} Tagen).\nZum Verlängern: http://opac.st-ingbert.de/webopac/index.asp?kontofenster=start")
+        mail_body = f"... am {first_date.strftime('%d. %m.')} (in weniger als {WARN_DAYS_IN_ADVANCE} Tagen).\n(Zum Verlängern: http://opac.st-ingbert.de/webopac/index.asp?kontofenster=start\n\n)"
+        extend_result = extend_books(books, books[0][2])
+        send_mail("Bücher laufen ab", mail_body + extend_result)
 
 
 if __name__ == "__main__":
