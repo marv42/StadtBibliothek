7e2046e5d91e2652326e2edb4ba8947e66dae654
Marvin <marv42+github@gmail.com>
2024-01-17 22:24:14 +0100

    Remove unused variable

--- StadtBibliothek.sh
+++ StadtBibliothek.sh
@@ -10,6 +10,5 @@ JUST_THE_LATEST_PART_OF_THE_LOG_FILE=$( tail -1000 $LOG_FILE )
 echo "$JUST_THE_LATEST_PART_OF_THE_LOG_FILE" > $LOG_FILE
 date >> $LOG_FILE
 echo DIR=$DIR >> $LOG_FILE
-echo SOURCE=$SOURCE >> $LOG_FILE
 
 $DIR/venv/bin/python3 $DIR/StadtBibliothek.py 2>&1 | tee -a $LOG_FILE
